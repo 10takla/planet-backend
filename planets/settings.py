976885e033af9ab20ca35865260f09ed7a8e243a
10takla <letifovabakar98@gmail.com>
2023-06-07 17:07:01 +0300

    create app and user

--- planets/settings.py
+++ planets/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$-^tqe+id#br*7no4+)yo-tpxugg)e_bkc3q-^*27j!^v7cetd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
